8b3f1681725f133ad61f3530fb1f1dc2bf9e4523
hemantsb <hemant.bisht@outlook.com>
2022-01-09 14:42:01 +0530

    initial commit

---
+++ common/src/commonMain/kotlin/bit/hemant/note_kmm/Greeting.kt
@@ -0,0 +1,7 @@
+package bit.hemant.note_kmm
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ common/src/commonMain/kotlin/bit/hemant/note_kmm/Platform.kt
@@ -0,0 +1,5 @@
+package bit.hemant.note_kmm
+
+expect class Platform() {
+    val platform: String
+}
